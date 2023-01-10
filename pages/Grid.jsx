6133ef2bc8c83bb872788c9f48c2bed836e50524
esther921 <estherniyonkuru51@gmail.com>
2023-01-10 18:20:13 +0200

    Made a non responsive desktop version

---
+++ pages/Grid.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <div className="grid grid-cols-8 gap-4 max-w-7xl mx-auto">
+      <div className="col-span-2 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-3 row-span-3">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-1 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-3">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className=" col-span-1 row-span-3">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-3 row-span-3">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-1 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-2 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-1 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+      <div className="col-span-1 row-span-2">
+        {" "}
+        <img
+          className="h-full w-full object-cover aspect-video"
+          src="https://nexter.netlify.app/img/gal-2.jpeg "
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
